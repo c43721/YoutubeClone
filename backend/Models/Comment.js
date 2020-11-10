e0381baf8710d17c39fa79eb51058c0f9553bb03
Timothy Casey <tvcasey@yahoo.com>
2020-11-09 22:30:13 -0500

    Found some more code to help Ryan code the comments component of our front end.  Saw an others category that takes in the undefined information from the api.  Also, found a async/await function to find all the comments.

--- backend/Models/Comment.js
+++ backend/Models/Comment.js
@@ -13,11 +13,22 @@ const CommentSchema = new mongoose.Schema({
       type: [mongoose.Schema.Types.ObjectId],
   },
   date: {
-      type: Date, default: Date.now
+      type: Date, default: Date.now,
   },
-  time: {
-      type: time, default: time.now
+  others: {
+    type: Schema.Type.Mixed, // I don't think this is coded right, but it takes in all the data not programmed.
   },
 });
 
+//  I found this function from the class that may
+//  be helpful for you.
+
+async function getComments() {
+    const comments = await Comment.find();
+    console.log(comments);
+}
+getComments();
+
 module.exports = Comment = mongoose.model('comment', CommentSchema);
+
+
