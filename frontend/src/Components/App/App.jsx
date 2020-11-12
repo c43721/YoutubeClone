834fdec696bd3cd631d12368a991004a17a6e657
Timothy Casey <tvcasey@yahoo.com>
2020-11-12 17:50:35 -0500

    Found a bootstrap Modal that may be good to revise into a commet.

--- frontend/src/Components/App/App.jsx
+++ frontend/src/Components/App/App.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Axios from "axios";
+import 'bootstrap/dist/css/bootsstrrap.min.css';
+import { Modal } from 'bootstrap';
+
 
 const API_URL = "http://localhost:3001/";
 
@@ -18,5 +21,19 @@ export default function App() {
     <div>
       {response}
     </div>
+    /*<div>  
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Enter Your Comments:</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Comments go here!</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save Changes</Button>
+        </Modal.Footer>
+        </Modal.Dialog>
+    </div>*/
   );
 }
