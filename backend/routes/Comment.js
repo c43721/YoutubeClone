a27f205bc47aafe21fa30f1d26ab97831eb95fef
Timothy Casey <tvcasey@yahoo.com>
2020-11-11 21:27:59 -0500

    Did a proper mongoose schema get comment, get comments, post comment.

--- backend/routes/Comment.js
+++ backend/routes/Comment.js
@@ -12,58 +12,32 @@ const Comment = require("../Models/Comment");
 //   });
 
 
+//GET ALL COMMENT:
+
+const comments = await Comment.find({ videoId, username, text })
+    .select({ text });
+    console.log(comments);
+
+//POST A COMMENT:
+
+const update = await Comment.findByIdAndUpdate('videoId', 
+    {
+        text: comment, 
+    },
+    { new: true }
+);
+console.log(update);
 
 
-//We need to fix the problem of having two Comment.js files, both in the backend.
-//This is a start on getting this info for you, hopefully to revise and make work.
-//I know your aysnc/await function may be better, but I like the simplicity of 
-//the functions that I found on youTube.  Of course, you can make a function instead
-//of Comment and maybe call it at the end.  I'm really bummed my tech is always
-//having problems.  You can always text me so I can use my phone.
 
-//GET REQUEST:
 
-getComment(); { //this needs a function, but this is the module/Component name.
-    axios({
-        method: 'get',
-        url: 'http://localhost:3001',
-        params: {
-            _limit: 5 //limits the list of comments to 5
-        }
-    })
-        .then(res => console.log(res))
-        .catch(err => console.error(err));
-}
-getComment();
+//GET A COMMENT:
 
-//POST REQUEST:
+const comment = await Comment.findById({ videoId })
+    .select({ text });
+    console.log(comment);
 
-postComment(); {
-    axios({
-        method: 'post',
-        url: 'http://localhost:3001',
-        data: {
-            videoId,  //we need a way to imput info for these categories.
-            username,
-            text
-        }
-    })
-        .then(res => console.log(res))
-        .catch(err => console.error(err));
-}
-postComment();
 
-putComment(); {
-    axios({
-        method: 'put',
-        url: 'http://localhost:3001/1',
-        data: {
-            videoId,  //we need a way to imput info for these categories.
-            username,
-            text
-        }
-    })
-        .then(res => console.log(res))
-        .catch(err => console.error(err));
-}
-putComment();
+
+
+module.exports = Router;
