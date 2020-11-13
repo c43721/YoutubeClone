693422bb6aae5547e94fea3d077e564d4948f01e
c43721 <rlahman.game@gmail.com>
2020-11-12 18:03:50 -0600

    Install react-bootstrap, start the comment form

--- frontend/src/Components/App/App.jsx
+++ frontend/src/Components/App/App.jsx
@@ -1,39 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import Axios from "axios";
-import 'bootstrap/dist/css/bootsstrrap.min.css';
-import { Modal } from 'bootstrap';
+import React, { useRef } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-
-const API_URL = "http://localhost:3001/";
+// const API_URL = "http://localhost:3001/";
 
 export default function App() {
-  const [response, setResponse] = useState();
-
-  useEffect(() => {
-    Axios.get(API_URL)
-      .then(response => {
-        const { message } = response.data;
-        setResponse(message)
-      })
-  }, []);
-
   return (
     <div>
-      {response}
+      Testing from Frontend
+      <FormElement />
     </div>
-    /*<div>  
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Enter Your Comments:</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Comments go here!</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save Changes</Button>
-        </Modal.Footer>
-        </Modal.Dialog>
-    </div>*/
+  );
+}
+
+function FormElement() {
+  const usernameRef = useRef();
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    const submittedUsername = usernameRef.current.value;
+    console.log(submittedUsername);
+    console.log("Form submitted!");
+  };
+
+  return (
+    <Form onSubmit={(e) => onSubmitForm(e)}>
+      <Form.Group controlId="usernamefield">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Your username"
+          ref={usernameRef}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="commentfield">
+        <Form.Label>Comment</Form.Label>
+        <Form.Control type="text" placeholder="I like this video a lot!" />
+      </Form.Group>
+
+      <Form.Group controlId="videoidfield">
+        <Form.Label>Video ID</Form.Label>
+        <Form.Control type="text" placeholder="542h51o641" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
