3641ca0e8eb74f260418a25cb2ae6253f68cf791
Timothy Casey <tvcasey@yahoo.com>
2020-11-12 19:23:37 -0500

    Trying to pull.

--- frontend/src/Components/App/App.jsx
+++ frontend/src/Components/App/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Axios from "axios";
-import 'bootstrap/dist/css/bootsstrrap.min.css';
+//import 'bootstrap/dist/css/bootsstrrap.min.css';
 import { Modal } from 'bootstrap';
 
 
