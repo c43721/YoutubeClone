ff49aab40ecc0105b43847cc193bd610390f279d
Timothy Casey <tvcasey@yahoo.com>
2020-11-12 22:27:36 -0500

    Helped to connect the Form to JSX variables and got them to enter into the console.  Also, put a axios get request.  Need to do an axios post request and tie it to the backend to the frontend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
 import './index.css';
+import $ from 'popper.js';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
   <React.StrictMode>
